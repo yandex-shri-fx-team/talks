1666a639c7a1e46290006d3374494c7b3e7b1c40
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-05-23 19:41:18 +0300

    feat(shrikaton-4): done

--- src/shrikaton-4/index.js
+++ src/shrikaton-4/index.js
@@ -12,7 +12,9 @@ ymaps.ready(() => {
     });
 
     ymaps.modules.require(['Polygonmap'], (Polygonmap) => {
-        const polygonmap = new Polygonmap({polygons, points});
+        const polygonmap = new Polygonmap({polygons, points}, {
+            colorOpacity: 0.8
+        });
 
         polygonmap.setMap(myMap);
     });
@@ -28,7 +30,8 @@ ymaps.ready(() => {
 
     ymaps.modules.require(['Polygonmap'], (Polygonmap) => {
         const polygonmap = new Polygonmap({polygons, points}, {
-            filterEmptyPolygons: true
+            filterEmptyPolygons: true,
+            colorOpacity: 0.8
         });
 
         polygonmap.setMap(myMap);
@@ -47,7 +50,8 @@ ymaps.ready(() => {
         const polygonmap = new Polygonmap({polygons, points}, {
             filter: function (object) {
                 return object.properties.pointsCount > 10;
-            }
+            },
+            colorOpacity: 0.8
         });
 
         polygonmap.setMap(myMap);
@@ -67,6 +71,100 @@ ymaps.ready(() => {
             colorScheme: 'summer',
             strokeWidth: 1,
             strokeColor: '#666',
+            colorOpacity: 0.8
+        });
+
+        polygonmap.setMap(myMap);
+    });
+});
+
+ymaps.ready(() => {
+    // eslint-disable-next-line no-unused-vars
+    const myMap = new ymaps.Map('map-5', {
+        center: [37.64, 55.76],
+        zoom: 10,
+        controls: ['zoomControl', 'typeSelector']
+    });
+
+    ymaps.modules.require(['Polygonmap'], (Polygonmap) => {
+        const polygonmap = new Polygonmap({polygons, points}, {
+            colorScheme: ['#4a148c', '#6a1b9a', '#7b1fa2', '#8e24aa', '#9c27b0', '#ab47bc', '#ba68c8', '#ce93d8', '#e1bee7', '#f3e5f5'],
+            colorRanges: 10,
+            colorOpacity: 0.8,
+            strokeWidth: 1,
+            strokeColor: '#666'
+        });
+
+        polygonmap.setMap(myMap);
+    });
+});
+
+ymaps.ready(() => {
+    const arr = [100, 200, 300, 400, 500, 600, 700];
+    let weightPoints = {};
+    weightPoints.features = points.features.map((el) => {
+        let rand = Math.floor(Math.random() * arr.length);
+        el.properties.weight = arr[rand];
+
+        return el;
+    });
+
+    weightPoints.type = 'FeatureCollection';
+    // eslint-disable-next-line no-unused-vars
+    const myMap = new ymaps.Map('map-6', {
+        center: [37.64, 55.76],
+        zoom: 10,
+        controls: ['zoomControl', 'typeSelector']
+    });
+
+    ymaps.modules.require(['Polygonmap'], (Polygonmap) => {
+        const polygonmap = new Polygonmap({polygons, points: weightPoints}, {
+            colorBy: 'weight',
+            colorByWeightType: 'middle',
+            colorOpacity: 0.8
+        });
+
+        polygonmap.setMap(myMap);
+    });
+});
+
+ymaps.ready(() => {
+    // eslint-disable-next-line no-unused-vars
+    const myMap = new ymaps.Map('map-7', {
+        center: [37.64, 55.76],
+        zoom: 10,
+        controls: ['zoomControl', 'typeSelector']
+    });
+
+    ymaps.modules.require(['Polygonmap'], (Polygonmap) => {
+        const polygonmap = new Polygonmap({polygons, points}, {
+            onClick(e) {
+                alert('Работает!');
+            },
+            colorOpacity: 0.8
+        });
+
+        polygonmap.setMap(myMap);
+    });
+});
+
+ymaps.ready(() => {
+    // eslint-disable-next-line no-unused-vars
+    const myMap = new ymaps.Map('map-9', {
+        center: [37.64, 55.76],
+        zoom: 10,
+        controls: ['zoomControl', 'typeSelector']
+    });
+
+    ymaps.modules.require(['Polygonmap'], (Polygonmap) => {
+        const polygonmap = new Polygonmap({polygons, points}, {
+            balloonContent(object) {
+                return `<div>
+                        <h3>Велопарковки Москвы</h3>
+                        <div>${object.properties.NAME}</div>
+                        <div>Количество парковок: <b>${object.properties.pointsCount}</b></div>
+                </div>`;
+            },
             colorOpacity: 0.8
         });
 
